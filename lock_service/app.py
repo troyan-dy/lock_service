a389afe0e3bf273e99bd2655f5cc6432dfb1c89b
troyan-dy <troyan-dy@sber-zvuk.com>
2021-03-20 22:34:18 +0300

    change port

--- lock_service/app.py
+++ lock_service/app.py
@@ -35,4 +35,4 @@ def create_app() -> FastAPI:
 
 if __name__ == "__main__":
     app = create_app()
-    uvicorn.run(app=app, host="0.0.0.0", port=8000)
+    uvicorn.run(app=app, host="0.0.0.0", port=5000)
