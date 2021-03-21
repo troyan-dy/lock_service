f7ad386e32e39b55b652e8286fe6829987633761
troyan-dy <troyan-dy@sber-zvuk.com>
2021-03-21 14:18:30 +0300

    add aiohttp_example

--- examples/aiohttp_example.py
+++ examples/aiohttp_example.py
@@ -1,44 +1,3 @@
-# lock_service
-http lock service
-
-Ready for heroku
-
-## Dependencies
-- docker-compose
-- python 3.9
-
-## Commands
-### Install
-- `make install`
-
-### Run tests
-- `make test`
-### Linting
-- `make lint`
-
-### Formatting
-- `make format`
-
-### Start in docker
-- `make start`
-- `make stop`
-
-
-## Usage
-
-- take lock
-```bash
-curl -X POST http://0.0.0.0:8000/take?key=123
-```
-
-- put lock
-```bash
-curl -X POST http://0.0.0.0:8000/put?key=123
-```
-
-## Example with aiohttp
-
-```python
 import asyncio
 from contextlib import asynccontextmanager
 
@@ -73,18 +32,3 @@ async def main():
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-```
-- Output
-```bash
-start Task 1
-start Task 2
-Take lock 'any_lock'
-start do job at Task 1
-finish do job at Task 1
-Put lock 'any_lock'
-Take lock 'any_lock'
-start do job at Task 2
-finish do job at Task 2
-Put lock 'any_lock'
-```
